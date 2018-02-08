0137c4e948b4881a2e16fbc7236c41da335de383
Ktrygub <konstantin.trygub@gmail.com>
2018-02-08 12:14:40 +0200

    CRA + Eslint/Prettier

---
+++ heroes-and-monsters-app/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <h1 className="App-title">Heroes and Monsters</h1>
+    </header>
+  </div>
+)
+
+export default App
